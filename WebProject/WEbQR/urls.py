d73f66c1a3a8d04dd8b79bb5e0e8549c91df7a89
moonahod <158854801+moonahod@users.noreply.github.com>
2024-09-16 09:44:08 +0300

    Update urls.py

--- WebProject/WEbQR/urls.py
+++ WebProject/WEbQR/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home),
+    path('', views.home),
     path('guide/', views.guide),
     path('start/', views.start),
     path('code/', views.code)
